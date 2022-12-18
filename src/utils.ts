3d03af1cddd2a65ccd0fd9cdc39761540e55cf19
xct007 <hackerxct007@gmail.com>
2022-12-19 02:39:16 +0700

    Initial commit

---
+++ src/utils.ts
@@ -0,0 +1,34 @@
+export const BASE_URL = "https://kiryuu.id/";
+
+export const LATEST_URL =
+	"http://45.76.148.33:8080/api/kiryuu/v6/search?page=1&status&order=update&title&type&genres";
+export const SEARCH_URL = (q: string): string =>
+	`http://45.76.148.33:8080/api/kiryuu/v6/search/simple?page=1&s=${q}`;
+export const MANGA_URL = (l: string) =>
+	`http://45.76.148.33:8080/api/kiryuu/v6/manga?id=${l}`;
+export const MANGA_CHAPTER_URL = (l: string) =>
+	`http://45.76.148.33:8080/api/kiryuu/v6/chapter?id=${l}`;
+
+export interface Result {
+	succeed?: boolean;
+	mangas?: {
+		cover?: string;
+		title?: string;
+		url?: string;
+		genres?: any;
+		rating?: string;
+		type?: string;
+		last_chapter?: {
+			chapter?: string;
+		};
+		nsfw?: boolean;
+	}[];
+}
+export const Config = {
+	headers: {
+		"User-Agent": "Dart/2.8 (dart:io)",
+		"Content-Type": "application/json",
+		"Accept-Encoding": "gzip",
+		"Content-Length": 0,
+	},
+};
